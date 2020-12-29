9bff7d17a3c8f1bac93efcec3fc31a1422b9d512
leo <leo.bravo.rain@gmail.com>
2020-12-29 00:34:44 -0300

    add time prediction algorithm

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 from sklearn.linear_model import LinearRegression
 # from sklearn import tree
@@ -60,6 +61,8 @@ def main():
     uploaded_file = st.sidebar.file_uploader('Sube el archivo: ', type='csv', encoding='auto', key=None)
     st.set_option('deprecation.showfileUploaderEncoding', False)
 
+    user_input = st.sidebar.number_input("Tiempo a analizar: ", 0.0)
+
     if uploaded_file is not None:
 
         data = pd.read_csv(uploaded_file)
@@ -69,16 +72,51 @@ def main():
         # if st.sidebar.checkbox("Obtener tiempo de falla"):
         if st.sidebar.button("Obtener tiempo de falla"):
 
-            # Do all computing to get the prediction time
-            
-            # PASTE CODE HERE
+                # Do all computing to get the prediction time
+                
+                # PASTE CODE HERE
 
-            # get prediction time
-            predictionTime = 15
+                y = data.iloc[:, 0]
+                print(y.shape)
 
-            # dislplay prediction
-            st.markdown("La predicción del tiempo de falla es: {} [seg]".format(predictionTime))
+                # Specify the sample size
+                size = data.shape[0]
+                x = scipy.arange(size)
+                print(x.shape)
 
+                # get prediction time
+                predictionTime = 15
+
+
+                # Step 3: Obtain the distribution parameters
+
+                # Define the distribution with the best fit
+                dist = getattr(scipy.stats, 'gamma')
+
+                # Fit the distribution to the data
+                param = dist.fit(y)
+
+                # Distribution parameters (e.g. 'a', 'b' for beta distribution)
+                args = param[:-2]
+
+                # Location parameter
+                loc = param[-2]
+
+                # Scale parameter
+                scale = param[-1]
+
+                # display results
+                st.markdown("Probability of failure before time 300: " + str(round(scipy.stats.gamma.cdf(user_input, *args, loc=loc, scale=scale)*100,2)) + "%")
+
+                st.markdown("Reliability Estimation at time 300: " + str(round(scipy.stats.gamma.sf(user_input, *args, loc=loc, scale=scale)*100,2)) + "%")
+
+                st.markdown("Probability density function at time 300: " + str(round(scipy.stats.gamma.pdf(user_input, *args, loc=loc, scale=scale)*100,2)))
+
+                # print("Probability of failure before time 300:", round(scipy.stats.gamma.cdf(300, *args, loc=loc, scale=scale)*100,2),"%")
+
+                # print("Reliability Estimation at time 300:", round(scipy.stats.gamma.sf(300, *args, loc=loc, scale=scale)*100,2),"%")
+
+                # print("Probability density function at time 300", round(scipy.stats.gamma.pdf(300, *args, loc=loc, scale=scale)*100,2))
 
 if __name__ == "__main__":
     main()
